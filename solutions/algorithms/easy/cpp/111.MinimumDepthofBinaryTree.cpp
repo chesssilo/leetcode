04243a281c44b0670232c077e1da28d741c2b9ef
redhudy <art13git7@gmail.com>
2023-06-05 10:35:17 +0300

    Runtime 308 ms Beats 17.49% Memory 144.8 MB Beats 20.22%

---
+++ solutions/algorithms/easy/cpp/111.MinimumDepthofBinaryTree.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        if (root->left == nullptr)
+            return minDepth(root->right) + 1;
+        if (root->right == nullptr)
+            return minDepth(root->left) + 1;
+        return std::min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution solution;
+    int result = solution.minDepth(root);
+    std::cout << result << std::endl;
+
+    return 0;
+}
