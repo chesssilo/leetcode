d8703073f86e1df2ec31d919bb59fd84f110d506
redhudy <art13git7@gmail.com>
2023-08-04 12:16:39 +0300

    leetcode

---
+++ solutions/1873. Calculate Special Bonus/1873.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = np.where( (employees['employee_id'] % 2 == 0) | (employees['name'].str.match('M')),  0, employees['salary'])
+    df = employees[['employee_id','bonus']].sort_values(by=['employee_id'])
+    return df
