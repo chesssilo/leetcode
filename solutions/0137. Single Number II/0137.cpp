a8029c67da919122b67f8cc4f2ff9eed6af271e2
redhudy <art13git7@gmail.com>
2023-08-05 08:11:48 +0300

    leetcode

---
+++ solutions/0137. Single Number II/0137.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static const int EQUAL_COUNT = 3;
+
+    int singleNumber(vector<int>& nums) {
+        const int INT_BIT_SIZE = sizeof(int) * 8;
+        size_t countOnes[INT_BIT_SIZE] = {};
+        for (unsigned int x : nums) {
+            for (int bit = 0; bit < INT_BIT_SIZE; ++bit) {
+                countOnes[bit] += x & 1;
+                x >>= 1;
+            }
+        }
+
+        unsigned int ans = 0;
+        for (int bit = 0; bit < INT_BIT_SIZE; ++bit) {
+            ans |= (unsigned int)(countOnes[bit] % EQUAL_COUNT) << bit;
+        }
+        return ans;
+    }
+};
