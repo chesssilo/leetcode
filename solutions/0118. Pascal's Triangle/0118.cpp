072b865d9b983923bd11ed631eb2685b344421cf
redhudy <art13git7@gmail.com>
2023-09-08 07:39:05 +0300

    leetcode

--- solutions/0118. Pascal's Triangle/0118.cpp
+++ solutions/0118. Pascal's Triangle/0118.cpp
@@ -1,14 +1,16 @@
+// Time complexity: O(n^2)
+// Space complexity: O(n^2)
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
+        vector<vector<int>> ans(numRows);
 
-        for (int i = 0; i < numRows; ++i)
-            ans.push_back(vector<int>(i + 1, 1));
-
-        for (int i = 2; i < numRows; ++i)
-            for (int j = 1; j < ans[i].size() - 1; ++j)
+        for (int i = 0; i < numRows; ++i) {
+            ans[i].resize(i + 1, 1);
+            for (int j = 1; j < i; ++j) {
                 ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+        }
 
         return ans;
     }
