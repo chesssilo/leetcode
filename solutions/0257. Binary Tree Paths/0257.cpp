938384c470e94f315ff1db0092fdccb518c3cabe
redhudy <art13git7@gmail.com>
2023-09-10 11:26:03 +0300

    leetcode

---
+++ solutions/0257. Binary Tree Paths/0257.cpp
@@ -0,0 +1,23 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        visit(root, "", ans);
+        return ans;
+    }
+private:
+    void visit(TreeNode *node, string path, vector<string> &ans) {
+        if (node == nullptr) {
+            return;
+        }
+        if (node->left == nullptr && node->right == nullptr) {
+            ans.push_back(path + to_string(node->val));
+            return;
+        }
+        path += to_string(node->val) + "->";
+        visit(node->left, path, ans);
+        visit(node->right, path, ans);
+    }
+};
