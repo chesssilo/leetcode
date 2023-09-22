93257c3d6a711a32c425dcdc43678f677882a59b
redhudy <art13git7@gmail.com>
2023-09-22 08:15:41 +0300

    leetcode

---
+++ solutions/0392. Is Subsequence/0392.cpp
@@ -0,0 +1,16 @@
+// Time complexity: O(n)
+// Space complexity: O(1)
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.empty())
+            return true;
+
+        int i = 0;
+        for (const char c : t)
+            if (s[i] == c && ++i == s.length())
+                return true;
+
+        return false;
+    }
+};
---
+++ solutions/0392. Is Subsequence/0392.py
@@ -0,0 +1,10 @@
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
