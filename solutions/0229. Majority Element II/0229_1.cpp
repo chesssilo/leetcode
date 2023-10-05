2d76633a71fd34d0fd114074fc39139f0ab0f551
m4k1m4 <m4k1m4fun@gmail.com>
2023-10-05 07:57:52 +0300

    leetcode

---
+++ solutions/0229. Majority Element II/0229_1.cpp
@@ -0,0 +1,22 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+public:
+    vector<int> majorityElement(const vector<int>& nums) {
+        unordered_map<int,int> count;
+        vector<int> ans;
+        int maxValue = nums.size() / 3;
+
+        for (const int num : nums) {
+            count[num]++;
+        }
+        
+        for (const auto& pair : count) {
+            if (pair.second >= maxValue) {
+                ans.push_back(pair.first);
+            }
+        }
+
+        return ans;
+    }
+};
