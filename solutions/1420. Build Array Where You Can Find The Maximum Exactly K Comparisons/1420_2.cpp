ccc717e35383c419947b32e2c326119738c034b4
m4k1m4 <m4k1m4fun@gmail.com>
2023-10-07 09:27:38 +0300

    leetcode

---
+++ solutions/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons/1420_2.cpp
@@ -0,0 +1,34 @@
+// Time complexity: O(nmk)
+// Space complexity: O(nmk)
+class Solution {
+public:
+    int numOfArrays(int n, int m, int k) {
+        long long dp[n + 1][m + 1][k + 1];
+        long long prefix[n + 1][m + 1][k + 1];
+        memset(dp, 0, sizeof(dp));
+        memset(prefix, 0, sizeof(prefix));
+        int MOD = 1e9 + 7;
+        
+        for (int num = 1; num <= m; num++) {
+            dp[1][num][1] = 1;
+            prefix[1][num][1] = prefix[1][num - 1][1] + 1;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int maxNum = 1; maxNum <= m; maxNum++) {
+                for (int cost = 1; cost <= k; cost++) {
+                    long long ans = (maxNum * dp[i - 1][maxNum][cost]) % MOD;
+                    ans = (ans + prefix[i - 1][maxNum - 1][cost - 1]) % MOD;
+
+                    dp[i][maxNum][cost] += ans;
+                    dp[i][maxNum][cost] %= MOD;
+                    
+                    prefix[i][maxNum][cost] = (prefix[i][maxNum - 1][cost] + dp[i][maxNum][cost]);
+                    prefix[i][maxNum][cost] %= MOD;
+                }
+            }
+        }
+        
+        return prefix[n][m][k];
+    }
+};
