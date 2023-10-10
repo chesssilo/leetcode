e47b06f597a20fa0070c2db7d85e006881ca09b0
m4k1m4 <m4k1m4fun@gmail.com>
2023-10-10 08:44:11 +0300

    leetcode

---
+++ solutions/2009. Minimum Number of Operations to Make Array Continuous/2009_2.cpp
@@ -0,0 +1,27 @@
+// Time complexity: O(nlogn)
+// Space complexity: O(n)
+class Solution {
+public:
+    int minOperations(const vector<int>& nums) {
+        int n = nums.size();
+        int ans = n;
+        
+        set<int> unique(nums.begin(), nums.end());
+        vector<int> newNums;
+        for (int num : unique) {
+            newNums.push_back(num);
+        }
+        
+        int j = 0;
+        for (int i = 0; i < newNums.size(); i++) {
+            while (j < newNums.size() && newNums[j] < newNums[i] + n) {
+                j++;
+            }
+            
+            int count = j - i;
+            ans = min(ans, n - count);
+        }
+        
+        return ans;
+    }
+};
