0b918dd16eb8b6dbaafcf0fd82eb88a712d505e3
m4k1m4 <m4k1m4fun@gmail.com>
2023-10-30 09:31:11 +0300

    leetcode

---
+++ solutions/1356. Sort Integers by The Number of 1 Bits/1356_1.cpp
@@ -0,0 +1,17 @@
+// Time complexity: O(nsort)
+// Space complexity: O(sort)
+class Solution {
+public:
+    static bool compare(int a, int b) {
+        if (__builtin_popcount(a) == __builtin_popcount(b)) {
+            return a < b;
+        }
+        
+        return __builtin_popcount(a) < __builtin_popcount(b);
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+};
---
+++ solutions/1356. Sort Integers by The Number of 1 Bits/1356_2.cpp
@@ -0,0 +1,33 @@
+// Time complexity: O(nsort)
+// Space complexity: O(sort)
+class Solution {
+public:
+    static int findWeight(int num) {
+        int mask = 1;
+        int weight = 0;
+        
+        while (num > 0) {
+            if ((num & mask) > 0) {
+                weight++;
+                num ^= mask;
+            }
+            
+            mask <<= 1;
+        }
+        
+        return weight;
+    }
+    
+    static bool compare(int a, int b) {
+        if (findWeight(a) == findWeight(b)) {
+            return a < b;
+        }
+        
+        return findWeight(a) < findWeight(b);
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+};
