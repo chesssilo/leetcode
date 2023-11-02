970521d0e60e7991fb8e1cf5f9cc6d8037488794
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-02 07:35:53 +0300

    leetcode

---
+++ solutions/2265. Count Nodes Equal to Average of Subtree/2265.cpp
@@ -0,0 +1,29 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+public:
+    int averageOfSubtree(TreeNode* root) {
+        postOrder(root);
+        return count;
+    }
+private: 
+    int count = 0;
+    
+    pair<int, int> postOrder(TreeNode* root) {
+        if (root == NULL) {
+            return {0, 0};
+        }
+        
+        pair<int, int> left = postOrder(root->left);
+        pair<int, int> right = postOrder(root->right);
+        
+        int nodeSum = left.first + right.first + root->val;
+        int nodeCount = left.second + right.second + 1;
+
+        if (root->val == nodeSum / (nodeCount)) {
+            count++;
+        }
+        
+        return {nodeSum, nodeCount};
+    }
+};
