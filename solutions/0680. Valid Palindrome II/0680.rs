6c3871ecb6a9b632b4649cbf7d925f961ca7aa8d
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-06 09:41:30 +0300

    leetcode

---
+++ solutions/0680. Valid Palindrome II/0680.rs
@@ -0,0 +1,20 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+please write time and space complexity this algorithm: impl Solution {
+    pub fn valid_palindrome(s: String) -> bool {
+        let s = s.as_bytes();
+        let n = s.len();
+        for i in 0..n / 2 {
+            if s[i] != s[n-i-1] {
+                let l = &s[i..n-i-1];
+                let r = &s[i+1..n-i];
+                return Self::helper(l) || Self::helper(r);
+            }
+        }
+        true
+    }
+    fn helper(s: &[u8]) -> bool {
+        let n = s.len();
+        (0..n / 2).all(|i| s[i] == s[n-i-1])
+    }
+}
