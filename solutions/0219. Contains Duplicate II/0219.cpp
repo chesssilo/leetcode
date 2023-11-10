175aebe20f1c955e2b0b0532dd7afcac6474c6a3
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-10 11:19:49 +0300

    leetcode

---
+++ solutions/0219. Contains Duplicate II/0219.cpp
@@ -0,0 +1,18 @@
+// Time complexity: O(n)
+// Space complexity: O(k)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> seen;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!seen.insert(nums[i]).second) {
+                return true;
+            }    
+            if (i >= k) {
+                seen.erase(nums[i - k]);
+            }
+        }
+        return false;
+    }
+};
---
+++ solutions/0219. Contains Duplicate II/0219.rs
@@ -0,0 +1,21 @@
+// Time complexity: O(n)
+// Space complexity: O(k)
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut seen = HashSet::new();
+
+        for (i, &num) in nums.iter().enumerate() {
+            if !seen.insert(num) {
+                return true;
+            }
+            
+            if i as i32 >= k {
+                seen.remove(&(nums[i as usize - k as usize]));
+            }
+        }
+        
+        false
+    }
+}
