2e0fed3b098b8fc40331a83b7e03389923994a86
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-18 10:54:02 +0300

    leetcode

---
+++ solutions/1838. Frequency of the Most Frequent Element/1838_3.cpp
@@ -0,0 +1,45 @@
+// Time complexity: O(n*sort)
+// Space complexity: O(n)
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        ranges::sort(nums);
+        vector<long long> prefix;
+        prefix.push_back(nums[0]);
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            prefix.push_back(nums[i] + prefix.back());
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            ans = max(ans, check(i, k, nums, prefix));
+        }
+        
+        return ans;
+    }
+private:
+    int check(int i, int k, vector<int>& nums, vector<long long>& prefix) {
+        int target = nums[i];
+        int left = 0;
+        int right = i;
+        int best = i;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long count = i - mid + 1;
+            long finalSum = count * target;
+            int originalSum = prefix[i] - prefix[mid] + nums[mid];
+            int operationsRequired = finalSum - originalSum;
+            
+            if (operationsRequired > k) {
+                left = mid + 1;
+            } else {
+                best = mid;
+                right = mid - 1;
+            }
+        }
+        
+        return i - best + 1;
+    }
+};
