ada6b0d3468c629b52b9f47c3aa5cfcd4e63faa1
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-23 08:55:47 +0300

    leetcode

---
+++ solutions/1630. Arithmetic Subarrays/1630_1.cpp
@@ -0,0 +1,27 @@
+// Time complexity: O(m⋅n⋅logn)
+// Space complexity: O(n)
+class Solution {
+public:
+    vector<bool> checkArithmeticSubarrays(const vector<int>& nums, const vector<int>& l, const vector<int>& r) {
+        vector<bool> ans;
+        for (int i = 0; i < l.size(); ++i) {
+            vector<int> arr(begin(nums) + l[i], begin(nums) + r[i] + 1);
+            ans.push_back(check(arr));
+        }
+        
+        return ans;
+    }
+private:
+    bool check(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int diff = arr[1] - arr[0];
+        
+        for (int i = 2; i < arr.size(); i++) {
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
---
+++ solutions/1630. Arithmetic Subarrays/1630_2.cpp
@@ -0,0 +1,43 @@
+// Time complexity: O(m⋅n)
+// Space complexity: O(n)
+class Solution {
+public:
+    vector<bool> checkArithmeticSubarrays(const vector<int>& nums, const vector<int>& l, const vector<int>& r) {
+        vector<bool> ans;
+        for (int i = 0; i < l.size(); ++i) {
+            vector<int> arr(begin(nums) + l[i], begin(nums) + r[i] + 1);
+            ans.push_back(check(arr));
+        }
+        
+        return ans;
+    }
+private:
+    bool check(vector<int>& arr) {
+        int minElement = INT_MAX;
+        int maxElement = INT_MIN;
+        unordered_set<int> arrSet;
+        
+        for (int num : arr) {
+            minElement = min(minElement, num);
+            maxElement = max(maxElement, num);
+            arrSet.insert(num);
+        }
+        
+        if ((maxElement - minElement) % (arr.size() - 1) != 0) {
+            return false;
+        }
+        
+        int diff = (maxElement - minElement) / (arr.size() - 1);
+        int curr = minElement + diff;
+        
+        while (curr < maxElement) {
+            if (arrSet.find(curr) == arrSet.end()) {
+                return false;
+            }
+            
+            curr += diff;
+        }
+        
+        return true;
+    }
+};
