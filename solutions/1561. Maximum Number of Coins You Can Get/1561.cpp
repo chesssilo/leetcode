85f2aadcb79eedda69f35535c9642dd612432594
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-24 15:53:06 +0300

    leetcode

---
+++ solutions/1561. Maximum Number of Coins You Can Get/1561.cpp
@@ -0,0 +1,15 @@
+// Time complexity: O(sort)
+// Space complexity: O(sort)
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(), piles.end());
+        int ans = 0;
+        
+        for (int i = piles.size() / 3; i < piles.size(); i += 2) {
+            ans += piles[i];
+        }
+        
+        return ans;
+    }
+};
