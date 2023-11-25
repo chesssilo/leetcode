b5da954770ee7bd5d935bac0d16ea7a451e35127
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-25 16:46:42 +0300

    leetcode

---
+++ solutions/1685. Sum of Absolute Differences in a Sorted Array/1685_2.cpp
@@ -0,0 +1,26 @@
+// Time complexity: O(n)
+// Space complexity: O(1)
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(const vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        
+        int leftSum = 0;
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) {
+            int rightSum = totalSum - leftSum - nums[i];
+            
+            int leftCount = i;
+            int rightCount = n - 1 - i;
+            
+            int leftTotal = leftCount * nums[i] - leftSum;
+            int rightTotal = rightSum - rightCount * nums[i];
+            
+            ans.push_back(leftTotal + rightTotal);
+            leftSum += nums[i];
+        }
+        
+        return ans;
+    }
+};
