3523cfac459cfffcc604982d2420bde9fbff2bd1
m4k1m4 <m4k1m4fun@gmail.com>
2023-11-29 06:25:45 +0300

    leetcode

---
+++ solutions/0191. Number of 1 Bits/0191.py
@@ -0,0 +1,11 @@
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ans = 0
+
+        for i in range(32):
+            if (n >> i) & 1:
+                ans += 1
+
+        return ans
