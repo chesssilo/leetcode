2b3c4d80f936482786518b62311b168a5024b021
Nikolay Andreev <131771807+m4k1m4@users.noreply.github.com>
2024-01-06 16:40:27 +0300

    leetcode

---
+++ solutions/1235. Maximum Profit in Job Scheduling/1235.cpp
@@ -0,0 +1,28 @@
+// Time complexity: O(sort)
+// Space complexity: O(n)
+class Solution {
+public:
+    int jobScheduling(const vector<int>& startTime, const vector<int>& endTime, const vector<int>& profit) {
+        int numJobs = profit.size();
+        vector<tuple<int, int, int>> jobs(numJobs);
+
+        for (int i = 0; i < numJobs; ++i) {
+            jobs[i] = {endTime[i], startTime[i], profit[i]};
+        }
+        
+        sort(jobs.begin(), jobs.end());
+        vector<int> dp(numJobs + 1);
+
+        for (int i = 0; i < numJobs; ++i) {
+            auto [endTime, startTime, profit] = jobs[i];
+
+            int latestNonConflictJobIndex = upper_bound(jobs.begin(), jobs.begin() + i, startTime, [&](int time, const auto& job) -> bool {
+                return time < get<0>(job);
+            }) - jobs.begin();
+
+            dp[i + 1] = max(dp[i], dp[latestNonConflictJobIndex] + profit);
+        }
+
+        return dp[numJobs];      
+    }
+};
