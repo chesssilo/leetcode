2cb0b733b746085ed24453910bfabd380d2bde51
m4k1m4 <m4k1m4fun@gmail.com>
2024-01-08 07:47:01 +0300

    leetcode

---
+++ solutions/0938. Range Sum of BST/0938.cpp
@@ -0,0 +1,19 @@
+// Time complexity: O(n)
+// Space complexity: O(h)
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->val < low) {
+            return rangeSumBST(root->right, low, high);
+        }
+        if (root->val > high) {
+            return rangeSumBST(root->left, low, high);
+        }
+
+        return root->val + rangeSumBST(root->right, low, high) + 
+               rangeSumBST(root->left, low, high);
+    }
+};
