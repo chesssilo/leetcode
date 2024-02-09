4de24d610c638aa4df836dc79be6e4f734c9aa4a
m4k1m4 <m4k1m4fun@gmail.com>
2024-02-09 07:44:55 +0300

    leetcode

---
+++ solutions/0279. Perfect Squares/0279.rs
@@ -0,0 +1,21 @@
+// Time complexity: O(logn)
+// Space complexity: O(nlogn)
+impl Solution {
+    pub fn num_squares(n: i32) -> i32 {
+        let mut dp = vec![n; (n + 1) as usize];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for i in 2..=n {
+            for j in 1..=i {
+                if j * j > i {
+                    break;
+                }
+                dp[i as usize] = dp[i as usize]
+                                    .min(dp[(i - j * j) as usize] + 1);
+            }
+        }
+
+        dp[n as usize]
+    }
+}
