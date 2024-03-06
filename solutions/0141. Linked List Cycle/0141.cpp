d7be14c74579a4371af310aff2dece04d4ec40f2
nikolayli <m4k1m4fun@gmail.com>
2024-03-06 08:31:40 +0300

    leetcode

---
+++ solutions/0141. Linked List Cycle/0141.cpp
@@ -0,0 +1,18 @@
+// Time complexity: O(n)
+// Space complexity: O(1)
+class Solution {
+public:
+  bool hasCycle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast)
+        return true;
+    }
+        
+    return false;
+  }
+};
--- solutions/0141. Linked List Cycle/0141.py
+++ solutions/0141. Linked List Cycle/0141.py
@@ -1,15 +1,14 @@
 # Time complexity: O(n)
 # Space complexity: O(1)
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
+  def hasCycle(self, head: Optional[ListNode]) -> bool:
+    slow = head
+    fast = head
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+      if slow == fast:
+        return True
 
-            if slow == fast:
-                return True
-
-        return False
+    return False
