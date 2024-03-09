6cff10f0eb4bae77b922891e8ce4854df717df24
nikolayli <m4k1m4fun@gmail.com>
2024-03-09 04:36:36 +0300

    leetcode

--- solutions/0021. Merge Two Sorted Lists/0021.cpp
+++ solutions/0021. Merge Two Sorted Lists/0021.cpp
@@ -1,29 +1,15 @@
+// Time complexity: O(∣list1∣+∣list2∣)
+// Space complexity: O(1)
 class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* res = new ListNode(0);
-        ListNode* current = res;
-
-        while (true) {
-            if (list1 == NULL) {
-                current->next = list2;
-                break;
-            }
-
-            if (list2 == NULL) {
-                current->next = list1;
-                break;
-            } 
-            
-            if (list1->val < list2->val) {
-                current->next = list1;
-                list1 = list1->next;
-            } else {
-                current->next = list2;
-                list2 = list2->next;
-            }
-            current = current->next;
-        }
-        return res->next;
-    }
+ public:
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    if (!list1 || !list2)
+      return list1 ? list1 : list2;
+    
+    if (list1->val > list2->val)
+      swap(list1, list2);
+    list1->next = mergeTwoLists(list1->next, list2);
+    
+    return list1;       
+  }
 };
