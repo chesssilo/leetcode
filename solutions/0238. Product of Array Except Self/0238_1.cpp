c987357d239576f2bed21c119049014417267b26
nikolayli <m4k1m4fun@gmail.com>
2024-03-16 06:42:47 +0300

    leetcode

---
+++ solutions/0238. Product of Array Except Self/0238_1.cpp
@@ -0,0 +1,22 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+ public:
+  vector<int> productExceptSelf(const vector<int>& nums) {
+    const int n = nums.size();
+    vector<int> ans(n);
+    vector<int> prefix(n, 1);
+    vector<int> suffix(n , 1);
+
+    for (int i = 1; i < n; ++i)
+      prefix[i] = prefix[i - 1] * nums[i - 1];
+
+    for (int i = n - 2; i >= 0; --i)
+      suffix[i] = suffix[i + 1] * nums[i + 1];
+
+    for (int i = 0; i < n; ++i)
+      ans[i] = prefix[i] * suffix[i];
+
+    return ans;            
+  }
+};
