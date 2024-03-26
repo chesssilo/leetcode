e9f1040423590a5d5cfeb1bab87c9b5e4314a1ee
nikolayli <m4k1m4fun@gmail.com>
2024-03-26 08:21:13 +0300

    leetcode

--- solutions/0437. Path Sum III/0437.cpp
+++ solutions/0437. Path Sum III/0437.cpp
@@ -1,27 +1,28 @@
 // Time complexity: O(n)
 // Space complexity: O(h+n)
 class Solution {
-public:
-    int pathSum(TreeNode* root, int targetSum) {
-        unordered_map<long long, int> mpp;
-        mpp[0]=1;
-        return solve(root, targetSum, 0, mpp);
-    }
-private:
-    int solve(TreeNode* root, int k, long long sum, unordered_map<long long, int>& mpp){
-        if(root == nullptr)
-            return 0;
+ public:
+  int pathSum(TreeNode* root, int targetSum) {
+    unordered_map<long long, int> mpp;
+    mpp[0]=1;
+    return solve(root, targetSum, 0, mpp);
+  }
+ private:
+  int solve(TreeNode* root, int k, long long sum, unordered_map<long long, int>& mpp){
+    if(root == nullptr)
+      return 0;
             
-        sum+=root->val;
+    sum += root->val;
         
-        int count = mpp[sum - k];
-        mpp[sum]++;
+    int count = mpp[sum - k];
+    mpp[sum]++;
         
-        int left=solve(root->left, k, sum, mpp);
-        int right=solve(root->right, k, sum, mpp);
+    int left = solve(root->left, k, sum, mpp);
+    int right = solve(root->right, k, sum, mpp);
         
-        mpp[sum]--;
-        sum-=root->val;
-        return count+left+right;
-    }
+    mpp[sum]--;
+    sum -= root->val;
+
+    return count + left + right;
+  }
 };
