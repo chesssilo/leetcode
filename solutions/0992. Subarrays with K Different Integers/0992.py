144181b1b5a7f59be6aa7fb28b447d61bac89918
nikolayli <m4k1m4fun@gmail.com>
2024-03-31 08:05:20 +0300

    leetcode

---
+++ solutions/0992. Subarrays with K Different Integers/0992.py
@@ -0,0 +1,23 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+  def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+    def subarraysWithAtMostKDistinct(k: int) -> int:
+      ans = 0
+      count = collections.Counter()
+
+      l = 0
+      for r, num in enumerate(nums):
+        count[num] += 1
+        if count[num] == 1:
+          k -= 1
+        while k < 0:
+          count[nums[l]] -= 1
+          if count[nums[l]] == 0:
+            k += 1
+          l += 1
+        ans += r - l + 1
+
+      return ans
+
+    return subarraysWithAtMostKDistinct(k) - subarraysWithAtMostKDistinct(k - 1)
