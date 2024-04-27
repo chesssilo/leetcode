f9da828dc7eaad30fbca32e56bc018602f55dbea
nikolayli <m4k1m4fun@gmail.com>
2024-04-27 06:32:59 +0300

    leetcode

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <div align="center">
 <br/>
-<img src="https://img.shields.io/badge/Solved-536/3125%20=%2017%25-blue.svg?style=flat-square" />
+<img src="https://img.shields.io/badge/Solved-537/3125%20=%2017%25-blue.svg?style=flat-square" />
 <br/>
 <img src="https://img.shields.io/badge/Easy-230/788-5CB85D.svg?style=flat-square" />
 <img src="https://img.shields.io/badge/Medium-236/1641-F0AE4E.svg?style=flat-square" />
--- solutions/1282. Group the People Given the Group Size They Belong To/1282.cpp
+++ solutions/1282. Group the People Given the Group Size They Belong To/1282.cpp
@@ -1,25 +1,25 @@
 // Time complexity: O(n)
 // Space complexity: O(n)
 class Solution {
-public:
-    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        unordered_map<int, vector<int>> groups;
-        vector<vector<int>> result;
-    
-        for (int i = 0; i < groupSizes.size(); ++i) {
-            int size = groupSizes[i];
-        
-            if (groups.find(size) == groups.end()) {
-                groups[size] = {i};
-            } else {
-                groups[size].push_back(i);
-            }
-        
-            if (groups[size].size() == size) {
-                result.push_back(groups[size]);
-                groups[size].clear();
-            }    
-        }    
-        return result;
+ public:
+  vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+    vector<vector<int>> ans;
+    unordered_map<int, vector<int>> groupSizeToIndices;
+
+    for (int i = 0; i < groupSizes.size(); ++i)
+      groupSizeToIndices[groupSizes[i]].push_back(i);
+
+    for (const auto& [groupSize, indices] : groupSizeToIndices) {
+      vector<int> groupIndices;
+      for (const int index : indices) {
+        groupIndices.push_back(index);
+        if (groupIndices.size() == groupSize) {
+          ans.push_back(groupIndices);
+          groupIndices.clear();
+        }
+      }
     }
+
+    return ans;
+  }
 };
