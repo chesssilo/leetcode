c9bb231b8f36b3da981e7389e784afe3821a4cdf
nikolayli <m4k1m4fun@gmail.com>
2024-05-07 06:26:55 +0300

    leetcode

---
+++ solutions/2487. Remove Nodes From Linked List/2487.py
@@ -0,0 +1,8 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+  def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head:
+      return None
+    head.next = self.removeNodes(head.next)
+    return head.next if head.next and head.val < head.next.val else head
