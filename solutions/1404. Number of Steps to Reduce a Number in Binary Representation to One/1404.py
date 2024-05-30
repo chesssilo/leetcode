9b6ebf260843b2866eeaf0ad7f6ab4f47d9a893a
nikolayli <m4k1m4fun@gmail.com>
2024-05-30 05:59:33 +0300

    leetcode

---
+++ solutions/1404. Number of Steps to Reduce a Number in Binary Representation to One/1404.py
@@ -0,0 +1,15 @@
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+  def numSteps(self, s: str) -> int:
+    ans = 0
+    chars = [c for c in s]
+
+    while chars[-1] == '0':
+      chars.pop()
+      ans += 1
+
+    if ''.join(chars) == '1':
+      return ans
+
+    return ans + 1 + sum(1 if c == '1' else 2 for c in chars)
