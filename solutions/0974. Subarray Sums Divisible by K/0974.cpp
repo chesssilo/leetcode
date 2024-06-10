c9661cd0831b22436c12d1e5d639e06265e202f7
nikolayli <m4k1m4fun@gmail.com>
2024-06-10 08:28:56 +0300

    leetcode

---
+++ solutions/0974. Subarray Sums Divisible by K/0974.cpp
@@ -0,0 +1,19 @@
+// Time complexity: O(n+k)
+// Space complexity: O(k)
+class Solution {
+ public:
+  int subarraysDivByK(const vector<int>& nums, int k) {
+    int ans = 0;
+    int prefix = 0;
+    vector<int> count(k);
+    count[0] = 1;
+
+    for (const int num : nums) {
+      prefix = (prefix + num % k + k) % k;
+      ans += count[prefix];
+      ++count[prefix];
+    }
+
+    return ans;
+  }
+};
---
+++ solutions/0974. Subarray Sums Divisible by K/0974.py
@@ -0,0 +1,15 @@
+# Time complexity: O(n+k)
+# Space complexity: O(k)
+class Solution:
+  def subarraysDivByK(self, nums: List[int], k: int) -> int:
+    ans = 0
+    prefix = 0
+    count = [0] * k
+    count[0] = 1
+
+    for num in nums:
+      prefix = (prefix + num % k + k) % k
+      ans += count[prefix]
+      count[prefix] += 1
+
+    return ans
