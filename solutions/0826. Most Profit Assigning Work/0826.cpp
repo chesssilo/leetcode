ebe51cc95e77675e8251a1c9afddc6f472932aa6
nikolayli <m4k1m4fun@gmail.com>
2024-06-19 04:26:47 +0300

    leetcode

---
+++ solutions/0826. Most Profit Assigning Work/0826.cpp
@@ -0,0 +1,28 @@
+// Time complexity: O(nlogn+mlogm)
+// Space complexity: O(n)
+class Solution {
+ public:
+  int maxProfitAssignment(const vector<int>& difficulty, 
+                          const vector<int>& profit, 
+                                vector<int>& worker) {
+    int ans = 0;
+    vector<pair<int, int>> jobs;
+
+    for (int i = 0; i < difficulty.size(); ++i)
+      jobs.emplace_back(difficulty[i], profit[i]);
+
+    ranges::sort(jobs);
+    ranges::sort(worker);
+
+    int i = 0;
+    int maxProfit = 0;
+
+    for (const int w : worker) {
+      for (; i < jobs.size() && w >= jobs[i].first; ++i)
+        maxProfit = max(maxProfit, jobs[i].second);
+      ans += maxProfit;
+    }
+
+    return ans;
+  }
+};
