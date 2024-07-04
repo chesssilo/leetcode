d7bed94ab3043567a9b579f024d5f46c05ac628d
nikolayli <m4k1m4fun@gmail.com>
2024-07-04 07:48:37 +0300

    leetcode

---
+++ solutions/1509. Minimum Difference Between Largest and Smallest Value in Three Moves/1509.py
@@ -0,0 +1,16 @@
+# Time complexity: O(sort)
+# Space complexity: O(sort)
+class Solution:
+  def minDifference(self, nums: List[int]) -> int:
+    n = len(nums)
+    if n < 5:
+      return 0
+
+    ans = math.inf
+
+    nums.sort()
+
+    for i in range(4):
+      ans = min(ans, nums[n - 4 + i] - nums[i])
+
+    return ans
