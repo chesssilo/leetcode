22fbd2d15bc3605fd7568252a20813981ca3bcbd
nikolayli <m4k1m4fun@gmail.com>
2024-08-15 06:14:26 +0300

    leetcode

--- solutions/0717. 1-bit and 2-bit Characters/0717.cpp
+++ solutions/0717. 1-bit and 2-bit Characters/0717.cpp
@@ -1,17 +1,17 @@
 // Time complexity: O(n)
 // Space complexity: O(1)
 class Solution {
-public:
-    bool isOneBitCharacter(const vector<int>& bits) {
-        const int n = bits.size();
+ public:
+  bool isOneBitCharacter(const vector<int>& bits) {
+    const int n = bits.size();
 
-        int i = 0;
-        while (i < n - 1)
-            if (bits[i] == 0)
-                i += 1;
-            else
-                i += 2;
+    int i = 0;
+    while (i < n - 1)
+      if (bits[i] == 0)
+        i += 1;
+      else
+        i += 2;
 
-        return i == n - 1;
-    }
+    return i == n - 1;
+  }
 };
