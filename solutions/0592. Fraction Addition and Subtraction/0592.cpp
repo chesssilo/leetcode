d28f930b585635dac3a40f65164e30ee908c8738
nikolayli <m4k1m4fun@gmail.com>
2024-08-24 13:30:59 +0300

    leetcode

---
+++ solutions/0592. Fraction Addition and Subtraction/0592.cpp
@@ -0,0 +1,23 @@
+// Time complexity: O(nlog(min(A,B)))
+// Space complexity: O(n)
+class Solution {
+ public:
+  string fractionAddition(string expression) {
+    istringstream iss(expression);
+    char _;
+    int a;
+    int b;
+    int A = 0;
+    int B = 1;
+
+    while (iss >> a >> _ >> b) {
+      A = A * b + a * B;
+      B *= b;
+      const int g = abs(__gcd(A,B));
+      A /= g;
+      B /= g;
+    }
+
+    return to_string(A) + "/" + to_string(B);
+  }
+};
---
+++ solutions/0592. Fraction Addition and Subtraction/0592.py
@@ -0,0 +1,16 @@
+# Time complexity: O(nlog(min(A,B)))
+# Space complexity: O(n)
+class Solution:
+  def fractionAddition(self, expression: str) -> str:
+    ints = list(map(int, re.findall('[+-]?[0-9]+', expression)))
+    A = 0
+    B = 1
+
+    for a, b in zip(ints[::2], ints[1::2]):
+      A = A * b + a * B
+      B *= b
+      g = math.gcd(A, B)
+      A //= g
+      B //= g
+
+    return str(A) + '/' + str(B)
