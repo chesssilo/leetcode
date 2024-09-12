e0959eebd6a6379bdf478d06f47108bf432ce7a0
nikolayli <m4k1m4fun@gmail.com>
2024-09-12 04:41:44 +0300

    leetcode

---
+++ solutions/2974. Minimum Number Game/2974.cpp
@@ -0,0 +1,13 @@
+// Time complexity: O(sort)
+// Space complexity: O(n)
+class Solution {
+ public:
+  vector<int> numberGame(vector<int>& nums) {
+    ranges::sort(nums);
+
+    for (int i = 0; i < nums.size(); i += 2)
+      swap(nums[i], nums[i + 1]);
+
+    return nums;
+  }
+};
---
+++ solutions/2974. Minimum Number Game/2974.py
@@ -0,0 +1,9 @@
+# Time complexity: O(sort)
+# Space complexity: O(n)
+class Solution:
+  def numberGame(self, nums: List[int]) -> List[int]:
+    nums.sort()
+
+    return [nums[i + 1] if i % 2 == 0
+            else nums[i - 1]
+            for i in range(len(nums))]
