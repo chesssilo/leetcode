e069cd8db26dc3ca5041c26709c58a05a3732ec7
nikolayli <m4k1m4fun@gmail.com>
2024-09-17 06:13:48 +0300

    leetcode

---
+++ solutions/3211. Generate Binary Strings Without Adjacent Zeros/3211.py
@@ -0,0 +1,20 @@
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+class Solution:
+  def validStrings(self, n: int) -> List[str]:
+    ans = []
+
+    def dfs(n: int, s: List[str]) -> None:
+      if n == 0:
+        ans.append(''.join(s))
+        return
+      if not s or s[-1] == '1':
+        s.append('0')
+        dfs(n - 1, s)
+        s.pop()
+      s.append('1')
+      dfs(n - 1, s)
+      s.pop()
+
+    dfs(n, [])
+    return ans
