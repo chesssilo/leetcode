8d8b1d297285f2108e6c47b1f641d4bdc1a7df1b
nikolayli <m4k1m4fun@gmail.com>
2024-10-05 06:53:45 +0300

    leetcode

---
+++ solutions/2491. Divide Players Into Teams of Equal Skill/2491.cpp
@@ -0,0 +1,24 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+ public:
+  long long dividePlayers(const vector<int>& skill) {
+    const int n = skill.size();
+    const int teamSkill = accumulate(skill.begin(), skill.end(), 0) / (n / 2);
+    long ans = 0;
+    unordered_map<int, int> count;
+
+    for(const int s : skill)
+      ++count[s];
+
+    for (const auto& [s, freq] : count) {
+      const int requiredSkill = teamSkill - s;
+      if (const auto it = count.find(requiredSkill);
+          it == count.cend() || it->second != freq)
+        return -1;
+      ans += static_cast<long>(s) * requiredSkill * freq;
+    }
+
+    return ans / 2;
+  }
+};
---
+++ solutions/2491. Divide Players Into Teams of Equal Skill/2491.py
@@ -0,0 +1,16 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+  def dividePlayers(self, skill: List[int]) -> int:
+    n = len(skill)
+    teamSkill = sum(skill) // (n // 2)
+    ans = 0
+    count = collections.Counter(skill)
+
+    for s, freq in count.items():
+      requiredSkill = teamSkill - s
+      if count[requiredSkill] != freq:
+        return -1
+      ans += s * requiredSkill * freq
+
+    return ans // 2
