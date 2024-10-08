131d2ec501ca2483ca8565fbbd2c06a8d2b7e41f
nikolayli <m4k1m4fun@gmail.com>
2024-10-08 07:32:47 +0300

    leetcode

---
+++ solutions/2696. Minimum String Length After Removing Substrings/2696.cpp
@@ -0,0 +1,23 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+ public:
+  int minLength(string_view s) {
+    stack<char> stack;
+
+    for (const char c : s)
+      if (c == 'B' && match(stack, 'A'))
+        stack.pop();
+      else if (c == 'D' && match(stack, 'C'))
+        stack.pop();
+      else
+        stack.push(c);
+
+    return stack.size();
+  }
+
+ private:
+  bool match(const stack<char>& stack, int c) {
+    return !stack.empty() && stack.top() == c;
+  }
+};
