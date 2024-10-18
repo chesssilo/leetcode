06abfdd9b92b23878c2f27ce9be66cb1e00e13fd
nikolayli <m4k1m4fun@gmail.com>
2024-10-18 13:13:05 +0300

    leetcode

---
+++ solutions/0670. Maximum Swap/0670.py
@@ -0,0 +1,16 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+  def maximumSwap(self, num: int) -> int:
+    s = list(str(num))
+    dict = {c: i for i, c in enumerate(s)}
+
+    for i, c in enumerate(s):
+      for digit in reversed(string.digits):
+        if digit <= c:
+          break
+        if digit in dict and dict[digit] > i:
+          s[i], s[dict[digit]] = digit, s[i]
+          return int(''.join(s))
+
+    return num
