e7d57dae0184684fd2e4aa28b2a28a7a57334324
nikolayli <m4k1m4fun@gmail.com>
2024-11-13 08:26:12 +0300

    leetcode

---
+++ solutions/2070. Most Beautiful Item for Each Query/2070.py
@@ -0,0 +1,13 @@
+# Time complexity: O(sort+mlogn)
+# Space complexity: O(n)
+class Solution:
+  def maximumBeauty(
+    self, items: List[List[int]], queries: List[int]
+  ) -> List[int]:
+    prices, beauties = zip(*sorted(items))
+    maxBeautySoFar = [0] * (len(beauties) + 1)
+
+    for i, beauty in enumerate(beauties):
+      maxBeautySoFar[i + 1] = max(maxBeautySoFar[i], beauty)
+
+    return [maxBeautySoFar[bisect_right(prices, query)] for query in queries]
