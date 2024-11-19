b87b3e12e1cd57d93b7cd9525e0abc65bcdf9102
nikolayli <m4k1m4fun@gmail.com>
2024-11-19 15:57:21 +0300

    leetcode

---
+++ solutions/1652. Defuse the Bomb/1652.py
@@ -0,0 +1,25 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+  def decrypt(self, code: List[int], k: int) -> List[int]:
+    n = len(code)
+    ans = [0] * n
+
+    if k == 0:
+      return ans
+
+    summ = 0
+    start = 1 if k > 0 else n + k
+    end = k if k > 0 else n - 1
+
+    for i in range(start, end + 1):
+      summ += code[i]
+
+    for i in range(n):
+      ans[i] = summ
+      summ -= code[start % n]
+      start += 1
+      end += 1
+      summ += code[end % n]
+
+    return ans
