7bfb2c222c49cab14a5acb032cc96c787ff94de8
nikolayli <m4k1m4fun@gmail.com>
2025-01-02 15:11:09 +0300

    leetcode

--- solutions/0011. Container With Most Water/0011.cpp
+++ solutions/0011. Container With Most Water/0011.cpp
@@ -1,22 +1,21 @@
 // Time complexity: O(n)
 // Space complexity: O(1)
 class Solution {
-public:
-    int maxArea(const vector<int>& height) {
-        int ans = 0;
-        int l = 0;
-        int r = height.size() - 1;
+ public:
+  int maxArea(const vector<int>& height) {
+    int ans = 0;
+    int l = 0;
+    int r = height.size() - 1;
 
-        while (l < r) {
-            const int minHeight = min(height[l], height[r]);
-            ans = max(ans, minHeight * (r - l));
-            if (height[l] < height[r]) {
-                ++l;
-            } else {
-                --r;
-            }
-        }
-
-        return ans;
+    while (l < r) {
+      const int minHeight = min(height[l], height[r]);
+      ans = max(ans, minHeight * (r - l));
+      if (height[l] < height[r])
+        ++l;
+      else
+        --r;
     }
+
+    return ans;
+  }
 };
